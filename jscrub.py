8e07b07842d89fdbf207c8e13d52fa528cdc2ba3
tylerjordan <tjordan@juniper.net>
2018-05-15 12:59:42 -0400

    Starting programming of file scrubbing logic.

--- jscrub.py
+++ jscrub.py
@@ -40,6 +40,18 @@ def detect_env():
     # Statically defined files and logs
     template_file = os.path.join(dir_path, template_dir, "Template.conf")
 
+# Function for scrubbing a file
+def scrub_file(input_file, ipmap_file, term_file):
+    # Load file into a list
+    line_list = txt_to_list(txt_file)
+
+    # Check if file list contains content
+    if line_list:
+        for line in line_list:
+            # Check if line contains any of the IPs or terms we are looking for
+
+
+
 # START OF SCRIPT #
 if __name__ == '__main__':
     try:
@@ -51,19 +63,33 @@ if __name__ == '__main__':
     # User will either provide an input_file or a folder structure to walk through and scrub all files.
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", type=str, help="input ASCII-formatted filename")
-    parser.add_argument("recurse_folder", type=str, help="directory to be scrubbed")
-    parser.add_argument("-i" "--ipmap", type=str, help="ipmap support files")
+    parser.add_argument("-i" "--ipmap_file", type=str, help="ipmap support files")
     args = parser.parse_args()
 
     # Input arguments
     input_file = args.input_file
-    ipmap_file = args.ipmap
+    ipmap_file = args.ipmap_file
+
+    # IPMAP List Dictionary
+    # - IPs will have IP address and mask
+    # - Sorted least specific to most specific IP
+    # src_ip,dest_ip
+    # 10.10.10.0/24,20.20.20.0/24
+    # 10.10.10.1/32,20.20.20.1/32
+    ipmap_ld = []
 
     # Main Program Loop
     try:
+        # Load the ipmap file into a list dictionary, if it exists...
         if ipmap_file:
+            ipmap_ld = csvListDict(fileName)
+
+        # Run this if the argument is a directory...
+        if os.path.isdir(input_file):
             pass
+        # Otherwise, this is a file...
         else:
+
             quit()
     except KeyboardInterrupt:
         print 'Exiting...'
