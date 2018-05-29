a222370efab7c2d1abec93aeed7cbca2e71e038b
tylerjordan <tjordan@juniper.net>
2018-05-29 14:31:49 -0400

    Adding more code to parse files.

--- jscrub.py
+++ jscrub.py
@@ -47,6 +47,7 @@ def detect_env():
 # 2. Search for existing replacements, if they don't exist, create new ones
 # 3. Create substrings of the remaining text
 def scrub_file(input_file, ipmap_file=None, term_file=None):
+    # Regexs
     ipv4_regex = re.compile("([1][0-9][0-9]|[2][5][0-5]|[2][0-4][0-9]|[1][0-9][0-9]|[0-9][0-9]|[0-9])"
                                 "\.([1][0-9][0-9]|[2][5][0-5]|[2][0-4][0-9]|[1][0-9][0-9]|[0-9][0-9]|[0-9])"
                                 "\.([1][0-9][0-9]|[2][5][0-5]|[2][0-4][0-9]|[1][0-9][0-9]|[0-9][0-9]|[0-9])"
@@ -62,9 +63,43 @@ def scrub_file(input_file, ipmap_file=None, term_file=None):
                             "{1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9])"
                             "{0,1}[0-9]))")
     regexs = [ipv4_regex, ipv6_regex]
-    # Load file into a list
-    line_list = txt_to_list(input_file)
 
+    # Load source map file, if specified...
+    line_list = txt_to_list(input_file)
+    exclude_list = []
+    include_list = []
+    if ipmap_file:
+        line_list = txt_to_list(ipmap_file)
+        print "IPMAP FILE: {0}".format(ipmap_file)
+        # Get include info
+        on_include = False
+        # Loop over ipmap file and create an exclude list and include listdict
+        for line in line_list:
+            # Check if there is text on this line...
+            if line:
+                # Check for "INCLUDE" text...
+                if "INCLUDE" in line:
+                    on_include = True
+                # If we are in INCLUDE section...
+                elif on_include:
+                    src_dest = line.split(",")
+                    src_ip = src_dest[0].split("/")
+                    dest_ip = src_dest[1].split("/")
+                    dest_split = src_dest[1].split("/")
+                    include_list.append({"src_ip": src_ip[0], "dest_ip": dest_ip[0], "mask": dest_split[1]})
+                # If we are in EXCLUDE section...
+                else:
+                    if not "EXCLUDE" in line:
+                        exclude_list.append(line)
+    else:
+        print "IPMAP FILE: NOT DEFINED"
+    # Print exclude list
+    print "Exclude List: {0}".format(exclude_list)
+    # Print include list
+    print "Include List: {0}".format(include_list)
+
+    # Load targeted scrub file into a list
+    line_list = txt_to_list(input_file)
     # Check for content using provided regexs
     if line_list:
         for line in line_list:
@@ -82,7 +117,7 @@ def scrub_file(input_file, ipmap_file=None, term_file=None):
                     # This adds the line segment before the ip
                     new_line += line[frag_start:ipindex[0]]
                     # This adds the replacement ip
-                    get_replacement_ip(line[ipindex[0]:ipindex[1]], ipmap_file)
+                    get_replacement_ip(str(line[ipindex[0]:ipindex[1]]), include_list, exclude_list)
                     new_line += "100.100.1.1"
                     # Update the frag_start to last index
                     frag_start = ipindex[1]
@@ -95,58 +130,69 @@ def scrub_file(input_file, ipmap_file=None, term_file=None):
                 #print "Modified Line: {0}\n".format(line)
 
 # Checks the provided IP against an optional list of IPs or creates a random one
-def get_replacement_ip(raw_ip, ipmap_file):
-    print "IPMAP FILE: {0}".format(ipmap_file)
+def get_replacement_ip(raw_ip, include_list, exclude_list):
     masked = False
     mask = "32"
-    ip = str(raw_ip)
+    ip = raw_ip
+    ipmasked = raw_ip + "/" + mask
     # Determine if this is a masked IP, assume /32 if no mask
+    print "-"*60
     if ":" in raw_ip:
-        print "IP: {0} is an IPv6 address.".format(ip)
+        print "IP: {0} is an IPv6 address.".format(raw_ip)
         mask = "128"
         return "fe80::feeb:daed"
     elif "/" in raw_ip:
-        print "IP: {0} is masked.".format(ip)
+        print "IP: {0} is masked.".format(raw_ip)
         masked = True
+        ipmasked = raw_ip
         mask = raw_ip.split("/")[1]
         ip = raw_ip.split("/")[0]
     else:
-        print "IP: {0} is NOT masked.".format(ip)
+        print "IP: {0} is NOT masked.".format(raw_ip)
     print "Mask: {0}".format(mask)
 
-    # Check if this IP is part of the excluded list or matches one of the
-    line_list = txt_to_list(ipmap_file)
-    exclude_list = []
-    include_list = []
-    on_include = False
-    # Loop over ipmap file and create an exclude list and include listdict
-    for line in line_list:
-        # Check if there is text on this line...
-        if line:
-            # Check for "INCLUDE" text...
-            if "INCLUDE" in line:
-                on_include = True
-            # If we are in INCLUDE section...
-            elif on_include:
-                src_dest = line.split(",")
-                src_ip = src_dest[0]
-                dest_ip = src_dest[1]
-                include_list.append({"src_ip": src_dest[0], "dest_ip": src_dest[1]})
-            # If we are in EXCLUDE section...
+    # Matching procedure
+    if not is_excluded(exclude_list, ip):
+        mydict = is_included(include_list, ipmasked)
+        # This executes if is_included returns a match
+        if mydict:
+            print "IP: {0} - Returned: {1}".format(ip, mydict)
+            # This executes if the match was exact, meaning, no new entries are required
+            if ipmasked == mydict['src_ip']:
+                if masked:
+                    return mydict['src_ip'] + "/" + mydict['mask']
+                else:
+                    return mydict['src_ip']
+            # This executes if the match was not exact, meaning, we need an entry for this IP
             else:
-                if not "EXCLUDE" in line:
-                    exclude_list.append(line)
+                pass
+        # This executes if is_included doesn't return a match, an unmatched entry!
+        else:
+            print "IP: {0} - Unmatched IP".format(ip)
+    else:
+        print "IP: {0} - Matched Exclude".format(ip)
+
+# Check for IP in the list
+def is_excluded(exclude_list, ip):
     # Loop over exclude IPs
-    print "Execlude List: {0}".format(exclude_list)
+    matched = False
     for exc_ip in exclude_list:
+        # If a match on the IP is made, this IP will be skipped
         if IPAddress(ip) in IPNetwork(exc_ip):
-            print "MATCHED! IP: {0} matched IP: {1}".format(ip, exc_ip)
-            return None
+            matched = True
+            #print "MATCHED: {0} with: {1}".format(ip, exc_ip)
+    return matched
 
+# Check included, if false, create new entry , if true, return the dictionary match
+def is_included(include_list, ipmasked):
     # Loop over include IPs
-    #for inc_ip in include_list:
-    #    if IPNetwork(inc_ip["src_ip"])
-
+    mymatch = {}
+    for inc_ip in include_list:
+        # If a match on the IP is made, we will finish scanning options to see if a more closer match exists
+        if IPNetwork(ipmasked) in IPNetwork(inc_ip['src_ip'] + "/" + inc_ip['mask']):
+            #print "MATCHED: {0} with: {1}".format(ipmasked, inc_ip["src_ip"])
+            mymatch = inc_ip
+    return mymatch
 
 # START OF SCRIPT #
 if __name__ == '__main__':
